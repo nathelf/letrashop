09ca3f6c411a87156465609a7bdd38ea0590a3af
Marcos Augusto Campagnaro <38305986+Kyhaiu@users.noreply.github.com>
2023-12-04 23:22:10 -0300

    Delete placas directory

--- placas/app/ui/components/Badge/Badge.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { Badge as BadgeChad } from "@/components/ui/badge";
-
-export function Badge() {
-  return <BadgeChad>Exmplo de Badge</BadgeChad>;
-}
--- placas/app/ui/components/Button/Button.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { Button as ButtonChad } from "@/components/ui/button";
-
-export function Button() {
-  return <ButtonChad>Exemplo de Botão</ButtonChad>;
-}
